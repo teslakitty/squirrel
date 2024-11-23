882a179b23b0fd6f67e7ea26c385ed38133644e9
James <jameskuyt655@gmail.com>
2024-11-23 14:51:47 -0500

    Initial commit

---
+++ app/src/main/java/com/teslakitty/squirrel/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.teslakitty.squirrel
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Find the button and set a click listener
+        findViewById<Button>(R.id.about).setOnClickListener {
+            // Navigate to AboutActivity
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
